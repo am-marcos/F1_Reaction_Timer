e27c61578c3b4d93f97f6bbc36e138108c5bf91f
am-marcos <noelinemarcos@gmail.com>
2024-10-11 15:01:17 +0200

    feat: test réussis des endpoints register et login users

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import { connectDB } from './configs/connectionDB';
+import { port } from './configs/config';
+import authRoutes from './routes/authRoute';
+//import timerRoutes from './routes/timerRoutes';
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+//app.use('/api/reaction-time', timerRoutes);
+
+connectDB();
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
